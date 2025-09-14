7db5e73ded3fbc5a3d4251ac628148d12c063d4c
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-09-14 14:50:36 -0700

    added api

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // Require Libraries
 const express = require('express');
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "AIzaSyBSXljZJU4_54MljlyzshJz_pPh0MoCM7o", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+  });
 
 // App Setup
 const app = express();
@@ -22,16 +28,21 @@ app.set('views', './views');
 // Routes
 // 1
 app.get('/', (req, res) => {
-    console.log(req.query)
-    res.render('home')
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
   })
 //2
-app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
-    const name = req.params.name;
-    // render the greetings view, passing along the name
-    res.render('greetings', { name });
-  })
 
 // Start Server
 
